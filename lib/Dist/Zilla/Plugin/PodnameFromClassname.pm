31b61f5f2e17b2aa4925bc1b3b66b9317d8e1464
Erik Carlsson <info@code301.com>
2015-01-15 01:05:12 +0100

    Init

---
+++ lib/Dist/Zilla/Plugin/PodnameFromClassname.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Dist::Zilla::Plugin::PodnameFromClassname;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Plugin::PodnameFromClassname;
+
+=head1 DESCRIPTION
+
+Dist::Zilla::Plugin::PodnameFromClassname is ...
+
+=head1 SEE ALSO
+
+=cut

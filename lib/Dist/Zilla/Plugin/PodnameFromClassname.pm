02656a9f5508d02125bdc6cfdaf32a8a86851395
Erik Carlsson <info@code301.com>
2016-04-02 20:37:57 +0000

    Relax rules regarding empty lines around the PODCLASSNAME comment.

--- lib/Dist/Zilla/Plugin/PodnameFromClassname.pm
+++ lib/Dist/Zilla/Plugin/PodnameFromClassname.pm
@@ -1,11 +1,15 @@
-use 5.10.1;
+use 5.10.0;
+use strict;
+use warnings;
 
 # VERSION
 # ABSTRACT: Insert PODNAME for Moops classes.
 
 package Dist::Zilla::Plugin::PodnameFromClassname;
 
+
 use Moose;
+use namespace::autoclean;
 with ('Dist::Zilla::Role::FileMunger', 'Dist::Zilla::Role::FileFinderUser' => { default_finders => [':InstallModules']});
 
 sub munge_files {
@@ -17,17 +21,27 @@ sub munge_file {
     my $self = shift;
     my $file = shift;
     my $content = $file->content;
-    if($content !~ m{\v\h*#\h+PODCLASSNAME:?\h*\v.*?\v\h*(?:(?:class)|(?:library)|(?:namespace)|(?:role)) +([a-zA-Z][a-zA-Z0-9_]*(?:::[a-zA-Z][a-zA-Z0-9_]*)*)}ms) {
+
+    my $optional_ws = qr/[\t ]*/;
+
+    if($content !~ m{^$optional_ws# PODCLASSNAME:?$}ms) {
         $self->log_debug(["Skipping %s, no # PODCLASSNAME directive found", $file->name]);
         return;
     }
-
+    elsif($content !~ m{^$optional_ws(?:class|library|namespace|role) +([a-zA-Z][a-zA-Z0-9_]*(?:::[a-zA-Z][a-zA-Z0-9_]*)*)}ms) {
+        $self->log_debug(["Skipping %s, none of the keywords found", $file->name]);
+        return;
+    }
     my $classname = $1;
 
-    $content =~ s{(\v\h*)(#\h+PODCLASSNAME:?)(?=\h*\v)}{$1# PODNAME: $classname};
+    $content =~ s{^($optional_ws)# PODCLASSNAME:?$}{$1# PODNAME: $classname}ms;
+
     $self->log(["Inserting podname for $classname"]);
     $file->content($content);
+
 }
+
+__PACKAGE__->meta->make_immutable;
 
 1;
 
@@ -57,14 +71,15 @@ Results in:
 
 Dist::Zilla::Plugin::PodnameFromClassname is useful together with L<Moops> and L<Pod::Weaver>. Since Moops classes generally don't also have a C<package> statement C<Pod::Weaver> can't pick up the module name.
 
-Using this plugin and adding a C<# PODCLASSNAME> directive fixes that. It replaces that directive with C<# PODNAME: $classname>.
+Using this plugin and adding a C<# PODCLASSNAME> line fixes that (by replacing such a line with the standard C<# PODNAME: $classname> comment). There should be only one C<# PODCLASSNAME> comment per file.
 
-There are a few simple rules:
+The plugin picks the first line that starts with C<class>, C<role>, C<namespace> or C<library> and uses the package name given on that line.
 
-=for :list
-* There must be at least one line (of anything) before the C<# PODCLASSNAME> directive.
-* There must be at least one line (of anything) between the C<# PODCLASSNAME> directive and the C<class> statement.
-* The C<# PODCLASSNAME> directive must be before the C<class> statement.
+The comment line should match this regular expression:
+
+    qr/^[\t ]*# PODCLASSNAME:?$/ms
+
+Note: While this plugin was developed with L<Moops> in mind, it should work with any module that introduces any of those keywords.
 
 =head1 SEE ALSO
 
